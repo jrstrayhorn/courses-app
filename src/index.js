cfd547e579789da81fd3230d9334cce26092d4f6
JR Strayhorn <jrstrayhorn@gmail.com>
2017-12-21 12:55:28 -0500

    Setting up redux with react

--- src/index.js
+++ src/index.js
@@ -2,11 +2,22 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import configureStore from './store/configureStore';
+
+import { Provider } from 'react-redux';
+
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 import './styles/styles.css'; // webpack can import CSS files too!
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
