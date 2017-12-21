60a98ef70c368e3d517d6507d55ca2b7bd89f436
stepforwardtech <jrstrayhorn@gmail.com>
2017-12-21 16:23:12 -0500

    Finished redux flow; adding in thunk

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,29 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import * as courseActions from '../../actions/courseActions';
+import { bindActionCreators } from 'redux';
 
 class CoursesPage extends React.Component {
-  constructor(props, context) {
-    super(props, context);
-
-    this.state = {
-      course: { title: '' }
-    };
-
-    this.onTitleChange = this.onTitleChange.bind(this);
-    this.onClickSave = this.onClickSave.bind(this);
-  }
-
-  onTitleChange(event) {
-    const course = this.state.course;
-    course.title = event.target.value;
-    this.setState({ course: course });
-  }
-
-  onClickSave() {
-    this.props.dispatch(courseActions.createCourse(this.state.course));
-  }
-
   courseRow(course, index) {
     return <div key={index}>{course.title}</div>;
   }
@@ -33,14 +13,6 @@ class CoursesPage extends React.Component {
       <div>
         <h1>Courses</h1>
         {this.props.courses.map(this.courseRow)}
-        <h2>Add Course</h2>
-        <input
-          type="text"
-          onChange={this.onTitleChange}
-          value={this.state.course.title}
-        />
-
-        <input type="submit" value="Save" onClick={this.onClickSave} />
       </div>
     );
   }
@@ -51,4 +23,10 @@ function mapStateToProps(state, ownProps) {
     courses: state.courses
   };
 }
-export default connect(mapStateToProps)(CoursesPage);
+
+const mapDispatchToProps = dispatch => {
+  return {
+    actions: bindActionCreators(courseActions, dispatch)
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
