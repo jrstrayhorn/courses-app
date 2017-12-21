9f7cd34df069d476ac8c106141ab8f76ec4900da
stepforwardtech <jrstrayhorn@gmail.com>
2017-12-21 17:02:14 -0500

    Clearing noise from code

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -5,21 +5,6 @@ export function loadCoursesSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses: courses };
 }
 
-/*
-// Older way with Promises
-export function loadCourses() {
-  return async function(dispatch) {
-    return courseApi
-      .getAllCourses()
-      .then(courses => {
-        dispatch(loadCoursesSuccess(courses));
-      })
-      .catch(error => {
-        throw error;
-      });
-  };
-}*/
-
 // New ES7 async/await
 export const loadCourses = () => async dispatch => {
   try {
