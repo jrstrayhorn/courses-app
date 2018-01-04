1c800e481e2174a261e29295fc73d610b279f7ba
stepforwardtech <jrstrayhorn@gmail.com>
2018-01-04 08:58:52 -0500

    Adding code to allow Manage Course Page component to update state when props are changed via changes to Redux store

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -18,13 +18,12 @@ class ManageCoursePage extends React.Component {
     this.saveCourse = this.saveCourse.bind(this);
   }
 
-  /*
   componentWillReceiveProps(nextProps) {
     if (this.props.course.id != nextProps.course.id) {
       // necessary to populate form when existing course is loaded directly.
       this.setState({ course: Object.assign({}, nextProps.course) });
     }
-  }*/
+  }
 
   updateCourseState(event) {
     const field = event.target.name;
